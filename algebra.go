08c2b92e6837d6c908a78b6ac1dbd1027c08a1c2
CLL <96326019+CllsPy@users.noreply.github.com>
2024-12-03 15:41:51 +0000

    add sum and subtract funciton.

--- algebra.go
+++ algebra.go
@@ -1,11 +1,37 @@
 package main
 
-import "fmt"
+import(
+	"fmt"
+)
+
+func addVector(v1[]float32, v2[]float32){
+    rsum :=[]float32{}
+	for i := 0; i < len(v1); i++ {
+		//fmt.Println(v1[i])
+		rsum = append(rsum, (v1[i] + v2[i]))
+
+	}
+
+	fmt.Println(rsum)
+}
+
+func subVector(v1[]float32, v2[]float32){
+	subResult := []float32{}
+
+	for i:=0; i < len(v1); i++{
+		subResult = append(subResult, v1[i] - v2[i])
+	}
+
+	fmt.Println(subResult)
+}
+
 
 func main() {
 
-	arr := []int{1, 2, 3, 4}
-	fmt.Println(arr)
+	v1 :=[]float32{1, 2, 3}
+	v2 :=[]float32{4, 5, 6}
+	addVector(v1, v2)
+	subVector(v1, v2)
 
 
 }
