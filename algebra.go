525cc5e788162bdf1b0a976a47a4f96edb27fdf1
CLL <96326019+CllsPy@users.noreply.github.com>
2024-12-03 15:44:51 +0000

    make func return values.

--- algebra.go
+++ algebra.go
@@ -4,25 +4,27 @@ import(
 	"fmt"
 )
 
-func addVector(v1[]float32, v2[]float32){
-    rsum :=[]float32{}
+func addVector(v1[]float32, v2[]float32)[]float32{
+    sumResult :=[]float32{}
 	for i := 0; i < len(v1); i++ {
 		//fmt.Println(v1[i])
-		rsum = append(rsum, (v1[i] + v2[i]))
+		sumResult = append(sumResult, (v1[i] + v2[i]))
 
 	}
 
-	fmt.Println(rsum)
+	//fmt.Println(rsum)
+	return sumResult
 }
 
-func subVector(v1[]float32, v2[]float32){
+func subVector(v1[]float32, v2[]float32)[]float32{
 	subResult := []float32{}
 
 	for i:=0; i < len(v1); i++{
 		subResult = append(subResult, v1[i] - v2[i])
 	}
 
-	fmt.Println(subResult)
+	//fmt.Println(subResult)
+	return subResult
 }
 
 
@@ -30,8 +32,9 @@ func main() {
 
 	v1 :=[]float32{1, 2, 3}
 	v2 :=[]float32{4, 5, 6}
-	addVector(v1, v2)
-	subVector(v1, v2)
+
+	fmt.Println(addVector(v1, v2))
+	fmt.Println(subVector(v1, v2))
 
 
 }
